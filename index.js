ba120339b85bd21d12e9fd248b975d67ef20cf6c
phaybein <fabian.m.martinez@gmail.com>
2016-06-20 11:21:29 -0700

    pushing finished work for code test 1-5

--- index.js
+++ index.js
@@ -13,21 +13,75 @@ var days =
 ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 // sending output of information to html
-document.getElementById("demo").innerHTML = "Today is " + days[d.getDay()] + '.' + " The current time is " + h + " " + ampm + " : " + m + " : " + s;
+document.getElementById("time").innerHTML = "Today is " + days[d.getDay()] + '.' + 
+" The current time is " + h + " " + ampm + " : " + m + " : " + s;
 
 
 // triangle problem starting
-var side1 = 5; 
-var side2 = 6; 
-var side3 = 7; 
+function findArea() {
+var side1 = $('#side1').val(); 
+var side2 = $('#side2').val(); 
+var side3 = $('#side3').val();
 var perimeter = (side1 + side2 + side3)/2;
 var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));
-console.log(area);
-document.getElementById("triangle").innerHTML = area.toFixed(4);
+
+document.getElementById("triangle").innerHTML = "The area of your triangle would be " 
++ area.toFixed(4);
+};
+
+
+// Code for the display scroll
+function animate_string(id) 
+{
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0]; // assuming no other children
+    var text = textNode.data;
+
+setInterval(function () 
+{
+ text = text[text.length - 1] + text.substring(0, text.length - 1);
+  textNode.data = text;
+}, 100);
+}
+
+/*(function() {
+    var word = 'Origin Code Academy Rocks!';
+
+    document.getElementById('#scroll').innerHTML = word;
+
+    setInterval(function() {
+        var last = word.slice(-1);
+        var curr = word.slice(0,-1);
+        word = last+curr;
+        document.getElementById('#scroll').innerHTML = word;
+    },150);
+
+})();*/
+
+
+// leap year information
+function findYear() {
+var inputyear = $('#year').val();
+var x = (inputyear % 100 === 0) ? (inputyear % 400 === 0) : (inputyear % 4 === 0);  
+$("#leapYear").html('the year ' + inputyear + " " + "is " + x);
+}
+// alert(x);
+
 
 
 // this is where the temp will start
-function toCelsius(f) {
+/*function toCelsius(f) {
     return (5/9) * (f-32);
+}*/
+
+// Note that the Math.round method is used, so that the result will be returned as an integer.
+
+function convert(degree) {
+    if (degree == "C") {
+        F = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(F);
+    } else {
+        C = (document.getElementById("f").value -32) * 5 / 9;
+        document.getElementById("c").value = Math.round(C);
+    }
 }
-document.getElementById("temp").innerHTML = "The temperature in Celsius would be " + toCelsius(77);
